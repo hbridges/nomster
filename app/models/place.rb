b70fde152484593a23db1f882b25585d930c959f
Harvey Bridges <hdbridges11@gmail.com>
2015-02-07 07:03:55 +0000

    added validates to the form fields for length, presence and duplicated same name and address

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,9 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
-	validates :name, :presence => true
+	validates :name, :presence => true, :length => { :maximum => 50, :minimum => 3},
+	 :uniqueness => {:scope => [:address]}
+	validates :address, :presence => true, :length => { :maximum => 50, :minimum => 3},
+	:uniqueness => {:scope => [:name]}
+	validates :description, :presence => true, :length => { :maximum => 200, :minimum => 3}
+
 end
