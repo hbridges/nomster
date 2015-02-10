03d4134134ef7e46fc780eee769de5d1335f7040
Harvey Bridges <hdbridges11@gmail.com>
2015-02-10 04:06:26 +0000

    Added last picture to the index page , put the pictures in a box on show page and scaled it to fit better

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,7 +3,12 @@ class PhotosController < ApplicationController
 	def create
 		@place = Place.find(params[:place_id])
 		@place.photos.create(photo_params.merge(:user => current_user))
-		redirect_to place_path(@place)
+
+		if @place.valid?
+			redirect_to place_path(@place)
+		else
+			render :text => 'Please choose a picture', :status => :unprocessable_entity
+		end
 	end
 
 	def photo_params
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,7 @@ class Photo < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :place
 	mount_uploader :picture, PictureUploader
+
+	validates :picture, :presence => true 
+	validates :caption, :presence => true 
 end
