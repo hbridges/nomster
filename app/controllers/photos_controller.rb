88c2fedbe8b30e49940976632e6c4f6ac062a73a
Harvey Bridges <hdbridges11@gmail.com>
2015-03-05 04:32:25 +0000

    Setup photo modal to reload when an error occurs

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,12 +2,13 @@ class PhotosController < ApplicationController
 	before_action :authenticate_user!
 	def create
 		@place = Place.find(params[:place_id])
-		@place.photos.create(photo_params.merge(:user => current_user))
+		@photo = @place.photos.create(photo_params.merge(:user => current_user))
 
 		if @place.valid?
 			redirect_to place_path(@place)
 		else
-			render :text => 'Please choose a picture', :status => :unprocessable_entity
+			@comment = Comment.new
+			render "places/show" , :status => :unprocessable_entity
 		end
 	end
 
---
+++ test/factories.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :place do
+    name "Pizza Hut"
+    description "I love pizza"
+    address "12w state st"
+  end
+end
